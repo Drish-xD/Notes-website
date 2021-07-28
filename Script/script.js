adccadfcd753b347afee46f440e6bb04695647bd
Drish-xD <Drish.xd@gmail.com>
2021-07-28 16:05:01 +0530

    Dark mode button added

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,8 @@
 AOS.init();
+
+var toggle = document.querySelector(".theme-toggle");
+
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("dawn");
+  toggle.classList.toggle("dusk");
+});
