691d30f886d25d7cb64393461983dd6dcc31f65d
Drish <drish.xd@gmail.com>
2021-08-11 00:43:59 +0530

    Dark Theme Toggle

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,23 @@
+// intiialising AOS
+
 AOS.init();
+
+// Theme Toggler
+
+var toggler = document.querySelector('.theme-toggle');
+toggler.addEventListener('click', () => {
+    toggler.classList.toggle('dawn');
+    toggler.classList.toggle('dusk');
+    document.body.classList.toggle('light');
+    document.body.classList.toggle('dark');
+    document.querySelector('.hamburger-icon').classList.toggle('light');
+    document.querySelector('.hamburger-icon').classList.toggle('dark');
+    document.querySelector('.main-img').classList.toggle('normal');
+    document.querySelector('.main-img').classList.toggle('invert');
+    document.querySelector('footer').classList.toggle('light');
+    document.querySelector('footer').classList.toggle('dark');
+    document.querySelectorAll('.notes-link').forEach(function (element) {
+        element.classList.toggle("light");
+        element.classList.toggle("dark");
+    });
+});
