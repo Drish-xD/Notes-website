f441394174ed0c34c2f6850e7bb796099a9b9170
Drish-xD <Drish.xD@gmail.com>
2021-11-01 14:58:53 +0530

    saved dark mode

--- Script/script.js
+++ Script/script.js
@@ -6,23 +6,6 @@ AOS.init({
     once: false,
 });
 
-
-// Theme Toggler
-
-var toggler = document.querySelector('.theme-toggle');
-var selection = document.querySelectorAll('body, .drawer-list, .hamburger-icon, .notes-link, .btn, footer, .form input, .file-dummy');
-
-toggler.addEventListener('click', () => {
-    toggler.classList.toggle('dawn');
-    toggler.classList.toggle('dusk');
-    selection.forEach(function (element) {
-        element.classList.toggle("light");
-        element.classList.toggle("dark");
-    });
-    document.querySelector('.main-img').classList.toggle('normal');
-    document.querySelector('.main-img').classList.toggle('invert');
-});
-
 // Overflow hidden 
 
 var menu = document.querySelector('.hamburger-icon');
@@ -37,3 +20,57 @@ document.querySelectorAll('.drawer-list ul a').forEach(item => {
         document.querySelector('.hamburger-icon').click();
     }, 2000);
 });
+
+// Theme Toggler
+
+var toggler = document.querySelector('.theme-toggle');
+var selection = document.querySelectorAll('body, .drawer-list, .hamburger-icon, .notes-link, .btn, footer, .form input, .file-dummy');
+
+const changeThemeToDark = () => {
+    console.log("dark");
+    toggler.classList.remove('dawn');
+    toggler.classList.add('dusk');
+    selection.forEach(function (element) {
+        element.classList.remove("light");
+        element.classList.add("dark");
+    });
+    document.querySelector('.main-img').classList.remove('normal');
+    document.querySelector('.main-img').classList.add('invert');
+    localStorage.setItem("data-theme", "dark");
+}
+
+const changeThemeToLight = () => {
+    console.log("llight");
+    toggler.classList.add('dawn');
+    toggler.classList.remove('dusk');
+    selection.forEach(function (element) {
+        element.classList.add("light");
+        element.classList.remove("dark");
+    });
+    document.querySelector('.main-img').classList.add('normal');
+    document.querySelector('.main-img').classList.remove('invert');
+    localStorage.setItem("data-theme", "light");
+}
+
+toggler.addEventListener('click', () => {
+    let theme = localStorage.getItem('data-theme')
+    if (document.body.classList.contains("light")) {
+        changeThemeToDark()
+        console.log(theme);
+    } else {
+        changeThemeToLight()
+        console.log(theme);
+    }
+});
+
+window.onload = changetheme();
+
+function changetheme() {
+  const theme = localStorage.getItem('data-theme')
+
+  if (theme != null && theme === "dark") {
+    changeThemeToDark()
+  } else {
+    changeThemeToLight()
+  }
+}
